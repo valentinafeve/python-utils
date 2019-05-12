cc4eb89ebabdcc05af051e46b24660bfceabdde3
valntinaf <feruerevalentina@gmail.com>
2019-05-12 17:23:20 -0400

    Testing ssh

--- Carpetaaa/sendingemails.py
+++ Carpetaaa/sendingemails.py
@@ -3,11 +3,11 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email='spamemail123@gmx.com'
-password='spamemail123'
+email='serranopat@yandex.com'
+password='serranopat2'
 send_to_email='ruastabi@gmail.com'
 subject='e-mail subject'
-message='Message content'
+message='<!DOCTYPE html><html><head><title>This is a message</title></head><body><style type="text/css">	*{		padding: 0		color: white;		background: black;	}</style><a href="www.facebook.com">Just a test</a><div background=blue>	Hey, whats up</div></body></html>'
 
 msg= MIMEMultipart()
 msg['From']= email
@@ -16,11 +16,11 @@ msg['subject']=subject
 
 msg.attach(MIMEText(message, 'plain'))
 
-server= smtplib.SMTP('mail.gmx.com',587)
+server= smtplib.SMTP('smtp.yandex.com',465)
 server.starttls()
 print(server.login(email,password))
 text = msg.as_string()
-for x in range(10000000000):
+for x in range(2):
 	server.sendmail(email, send_to_email, text)
 
 server.quit()
