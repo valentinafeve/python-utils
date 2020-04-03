1c5352d095c1e1dbd25dc06e432f911a94abf124
valntinaf <feruerevalentina@gmail.com>
2020-04-03 15:22:31 -0500

    Topic extraction

---
+++ nlp/topic_extraction.py
@@ -0,0 +1,67 @@
+from gensim.test.utils import common_texts
+from gensim.corpora.dictionary import Dictionary
+from gensim.models import LdaModel
+import pandas as pd
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+import nltk
+
+stemmer = SnowballStemmer(language='english')
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+data = pd.read_csv('data/abcnews-date-text.csv', error_bad_lines=False);
+data_text = data[['headline_text']]
+data_text['index'] = data_text.index
+documents = data_text
+
+np.random.seed(2018)
+nltk.download('wordnet')
+
+doc_sample = documents[documents['index'] == 0].values[0][0]
+print('original document: ')
+words = []
+for word in doc_sample.split(' '):
+    words.append(word)
+print(words)
+print('\n\n tokenized and lemmatized document: ')
+print(preprocess(doc_sample))
+
+processed_docs = documents['headline_text'].map(preprocess)
+processed_docs[:10]
+
+dictionary = gensim.corpora.Dictionary(processed_docs)
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+bow_corpus[0]
+
+bow_doc_0 = bow_corpus[0]
+for i in range(len(bow_doc_0)):
+    print("Word {} (\"{}\") appears {} time.".format(bow_doc_0[i][0],
+                                               dictionary[bow_doc_0[i][0]],
+bow_doc_0[i][1]))
+
+# # Create a corpus from a list of texts
+# common_dictionary = Dictionary(common_texts)
+# common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
+# # Train the model on the corpus.
+# lda = LdaModel(common_corpus, num_topics=10)
