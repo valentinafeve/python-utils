9dac38af99bf5afb3ac50ccd44cc07bb4ce5cade
valntinaf <feruereru@gmail.com>
2020-06-06 08:54:35 -0500

    Word vectors working weird

--- word-vectors/vectors.py
+++ word-vectors/vectors.py
@@ -6,31 +6,30 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 DIMENSIONS = 2
-MAX_NUM = 3
+LIMIT = 20
+NEW_RATIO = 2
 
 f = open("./data/text", "r+")
 
 word_vectors = {}
 
-center = np.array([ MAX_NUM/2 for x in range(DIMENSIONS)])
-cont = 0
+center = np.array([ LIMIT/2 for x in range(DIMENSIONS)])
 for line in f.readlines():
+
     line = line.replace(",","")
     line = line.replace("\n","")
     words = line.split(" ")
 
-    random_array = np.array([ random.uniform(-1,1) for x in range(DIMENSIONS) ])
+    random_array = np.array([ random.uniform( 1 , NEW_RATIO ) for x in range(DIMENSIONS) ])
 
     for word in words:
-        new_vector = word_vectors.get(word, center + random_array)
+        new_vector = word_vectors.get( word , center) + random_array
+
         word_vectors[word.lower()] = new_vector
-    cont += 1
-    if False:
-        break;
 
 # Plot the data
-plt.ylim(0, MAX_NUM)
-plt.xlim(0, MAX_NUM)
+plt.ylim(-LIMIT, LIMIT)
+plt.xlim(-LIMIT, LIMIT)
 
 for k, v in word_vectors.items():
     plt.text(v[0], v[1], k)
